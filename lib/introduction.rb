3962dc73184ce5f5247daa9ef3a165241cb073e6
Marie Berry (Leugermann) <marie@creativeos.io>
2019-02-18 00:13:10 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,5 +2,6 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language
+def introduction_with_language(language)
+  puts ""
   it "takes in two arguments, a name and a language, and puts out a phrase using those arguments." do 
